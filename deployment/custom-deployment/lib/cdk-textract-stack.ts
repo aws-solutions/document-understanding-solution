bb1c50b236cc497393f291211185d72bd7675e13
Alex Chirayath <alexchir@amazon.com>
2020-03-16 10:22:36 -0700

    Lambda log and config changes

--- deployment/custom-deployment/lib/cdk-textract-stack.ts
+++ deployment/custom-deployment/lib/cdk-textract-stack.ts
@@ -194,12 +194,23 @@ export class CdkTextractStack extends cdk.Stack {
       }
     );
 
+    const asyncEncryptionKey = new kms.Key(this, 'asyncEncryptionKey', {
+      enableKeyRotation: true
+    });
+    const asyncEncryptionKeyUsageStatment = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ["*"],
+      actions: ["kms:GenerateDataKey","kms:Encrypt","kms:Decrypt"]
+    });
+    asyncEncryptionKeyUsageStatment.addServicePrincipal('sns.awsamazon.com')
+    
     // SNS Topic
     const jobCompletionTopic = new sns.Topic(
       this,
       this.resourceName("JobCompletion"),
       {
-        displayName: "Job completion topic"
+        displayName: "Job completion topic",
+
       }
     );
 
@@ -262,7 +273,8 @@ export class CdkTextractStack extends cdk.Stack {
       {
         visibilityTimeout: cdk.Duration.seconds(900),
         retentionPeriod: cdk.Duration.seconds(1209600),
-        encryption : QueueEncryption.KMS_MANAGED
+        encryption : QueueEncryption.KMS,
+        encryptionMasterKey : asyncEncryptionKey
       }
     );
     // trigger
@@ -692,6 +704,8 @@ export class CdkTextractStack extends cdk.Stack {
     documentsTable.grantReadWriteData(jobResultProcessor);
     documentsS3Bucket.grantReadWrite(jobResultProcessor);
     samplesS3Bucket.grantReadWrite(jobResultProcessor);
+    asyncEncryptionKey.grantEncryptDecrypt(jobResultProcessor);
+
     jobResultProcessor.addToRolePolicy(
       new iam.PolicyStatement({
         actions: ["textract:GetDocumentTextDetection","textract:GetDocumentAnalysis"],
