bbeaa26d11c88bc23407c647d6f582bee84646af
Alex Chirayath <alexchir@amazon.com>
2020-03-17 08:13:38 -0700

    Medical documents visible

--- deployment/custom-deployment/lib/cdk-textract-stack.ts
+++ deployment/custom-deployment/lib/cdk-textract-stack.ts
@@ -193,24 +193,12 @@ export class CdkTextractStack extends cdk.Stack {
         }
       }
     );
-
-    const asyncEncryptionKey = new kms.Key(this, 'asyncEncryptionKey', {
-      enableKeyRotation: true
-    });
-    const asyncEncryptionKeyUsageStatment = new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      resources: ["*"],
-      actions: ["kms:GenerateDataKey","kms:Encrypt","kms:Decrypt"]
-    });
-    asyncEncryptionKeyUsageStatment.addServicePrincipal('sns.awsamazon.com')
-    
     // SNS Topic
     const jobCompletionTopic = new sns.Topic(
       this,
       this.resourceName("JobCompletion"),
       {
-        displayName: "Job completion topic",
-
+        displayName: "Job completion topic"
       }
     );
 
@@ -272,9 +260,7 @@ export class CdkTextractStack extends cdk.Stack {
       this.resourceName("JobResults"),
       {
         visibilityTimeout: cdk.Duration.seconds(900),
-        retentionPeriod: cdk.Duration.seconds(1209600),
-        encryption : QueueEncryption.KMS,
-        encryptionMasterKey : asyncEncryptionKey
+        retentionPeriod: cdk.Duration.seconds(1209600)
       }
     );
     // trigger
@@ -704,8 +690,7 @@ export class CdkTextractStack extends cdk.Stack {
     documentsTable.grantReadWriteData(jobResultProcessor);
     documentsS3Bucket.grantReadWrite(jobResultProcessor);
     samplesS3Bucket.grantReadWrite(jobResultProcessor);
-    asyncEncryptionKey.grantEncryptDecrypt(jobResultProcessor);
-
+    
     jobResultProcessor.addToRolePolicy(
       new iam.PolicyStatement({
         actions: ["textract:GetDocumentTextDetection","textract:GetDocumentAnalysis"],
