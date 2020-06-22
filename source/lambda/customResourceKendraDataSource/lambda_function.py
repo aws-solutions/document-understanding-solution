fb72e2b409ced46433f4c5822e2cf9255e2e662c
Shivani Mehendarge <61472912+ShivaniMehendarge@users.noreply.github.com>
2020-06-22 10:08:06 -0700

    Adding custom resources for kendra (#52)
    * Add Custom Resource to create Kendra Index and data source
    
    adding kendra role arn to lambda functions
    
    add interactive option for kendra and update cdk version in DUS to 1.42.0
    
    fixing typos
    
    update log messages and code to check delete of index
    
    Adding processed pdfs and renaming files
    
    Modified on_update functions for the kendra index and data source
    
    fixing the on_update handler functions
    
    Add the enable kendra option in deploy
    
    * Add enable Kendra option
    
    * remove unnecessary comments
    
    * Fixing typos
    
    * Fixed typo and removed the redundant if condition

---
+++ source/lambda/customResourceKendraDataSource/lambda_function.py
@@ -0,0 +1,63 @@
+import boto3
+import os
+
+def on_create(event, context):
+    kendra_client = boto3.client('kendra')
+    kendraIndexId = os.environ['KENDRA_INDEX_ID']
+    print("creating an s3 data source and connector for kendra index id: {}".format(kendraIndexId))
+    dataBucketName = os.environ['DATA_BUCKET_NAME']
+    roleArn = os.environ['KENDRA_ROLE_ARN']
+
+    create_data_source_response = kendra_client.create_data_source(
+        Name='DUSCovidDataset',
+        IndexId= kendraIndexId,
+        Type='S3',
+        Configuration={
+            'S3Configuration': {
+                'BucketName': dataBucketName,
+            }
+        },
+        Description='Covid pdfs data',
+        RoleArn=roleArn
+    )
+    dataSourceId = create_data_source_response['Id']
+    print("Data source id {} for kendra index with id {} created.".format(dataSourceId, kendraIndexId))
+    print("syncing the s3 data source")
+    response = kendra_client.start_data_source_sync_job(
+        Id=dataSourceId,
+        IndexId=kendraIndexId
+    )
+    print("Started sync job for data source id :{}, the execution id of the sync job is :{}".format(dataSourceId,response['ExecutionId']))
+    return {'PhysicalResourceId' : dataSourceId}
+   
+def on_update(event, context):
+    kendra_client = boto3.client('kendra')
+    kendraIndexId = os.environ['KENDRA_INDEX_ID']
+    print("Updating the s3 data source and connector for kendra index id: {}".format(kendraIndexId))
+    dataBucketName = os.environ['DATA_BUCKET_NAME']
+    roleArn = os.environ['KENDRA_ROLE_ARN']
+    dataSourceId = event['PhysicalResourceId']
+    # Name of the data source cannot be updated. Hence we do not give the customers an option to do so.
+    try:
+        update_data_source_response = kendra_client.update_data_source(
+            Id=dataSourceId,
+            Name='DUSCovidDataset',
+            IndexId= kendraIndexId,
+            Configuration={
+                'S3Configuration': {
+                    'BucketName': dataBucketName,
+                }
+            },
+            Description='Covid pdfs data',
+            RoleArn=roleArn
+        )
+        return {'PhysicalResourceId' : dataSourceId}
+    except:
+        raise
+
+def lambda_handler(event, context):
+    print("event: {}".format(event))
+    request_type = event['RequestType']
+    if(request_type == 'Create'): return on_create(event, context)
+    if(request_type == 'Update'): return on_update(event, context)
+    # Delete case not required as deleting Kendra index deletes the data source as well.
