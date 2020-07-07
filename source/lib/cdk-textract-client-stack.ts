0932e04dae232a7392d85680170b1731cfd27ba1
George Price <gwprice@amazon.com>
2020-07-07 19:27:33 -0400

    added copyright license headers to every source .js, .ts, and .py file

--- source/lib/cdk-textract-client-stack.ts
+++ source/lib/cdk-textract-client-stack.ts
@@ -1,3 +1,17 @@
+
+/**********************************************************************************************************************
+ *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           *
+ *                                                                                                                    *
+ *  Licensed under the Apache License, Version 2.0 (the License). You may not use this file except in compliance    *
+ *  with the License. A copy of the License is located at                                                             *
+ *                                                                                                                    *
+ *      http://www.apache.org/licenses/LICENSE-2.0                                                                    *
+ *                                                                                                                    *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES *
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    *
+ *  and limitations under the License.                                                                                *
+ *********************************************************************************************************************/
+
 import { Construct, Stack, StackProps } from "@aws-cdk/core";
 import s3 = require("@aws-cdk/aws-s3");
 import s3deployment = require("@aws-cdk/aws-s3-deployment");
