749966594739cc9e6da05942d38a2aa1c6e6df22
Dumas <pierred@c4b301c672a3.ant.amazon.com>
2020-07-31 11:12:35 -0700

    added uncommented folder creation and added copyright notice

--- source/lambda/customResourceKendraDataSource/lambda_function.py
+++ source/lambda/customResourceKendraDataSource/lambda_function.py
@@ -1,3 +1,16 @@
+######################################################################################################################
+#  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                           #
+#                                                                                                                    #
+#  Licensed under the Apache License, Version 2.0 (the License). You may not use this file except in compliance      #
+#  with the License. A copy of the License is located at                                                             #
+#                                                                                                                    #
+#      http://www.apache.org/licenses/LICENSE-2.0                                                                    #
+#                                                                                                                    #
+#  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES #
+#  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    #
+#  and limitations under the License.                                                                                #
+#####################################################################################################################
+
 import boto3
 import os
 
@@ -23,8 +36,8 @@ def on_create(event, context):
     
     # create s3 folders
     s3_client = boto3.client('s3', region_name=os.environ['AWS_REGION'])
-    #s3_client.put_object(Bucket=os.environ['BULK_PROCESSING_BUCKET'], Key=('documentDrop' +'/'))
-    #s3_client.put_object(Bucket=os.environ['BULK_PROCESSING_BUCKET'], Key=('kendraPolicyDrop' +'/'))
+    s3_client.put_object(Bucket=os.environ['BULK_PROCESSING_BUCKET'], Key=('documentDrop' +'/'))
+    s3_client.put_object(Bucket=os.environ['BULK_PROCESSING_BUCKET'], Key=('kendraPolicyDrop' +'/'))
     
     print("copying covid pdfs and kendra policies to bulk processing bucket")
     
@@ -66,7 +79,6 @@ def on_create(event, context):
 
 
 def on_update(event, context):
-    # not in use
     return
 
 
