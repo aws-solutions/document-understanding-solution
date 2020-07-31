8bf0ff95c259788833d26df319d18ec407d289b4
Dumas <pierred@c4b301c672a3.ant.amazon.com>
2020-07-31 16:38:28 -0700

    reorganized the samples directories

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 source .env
 aws s3 cp ./samples/ s3://$SampleBucketName/public/samples/ --recursive
+
+aws s3 cp ./GeneralPublic/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
+aws s3 cp ./HealthcareProvider/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
+aws s3 cp ./Scientist/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
+
+aws s3 cp ./GeneralPublic/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
+aws s3 cp ./HealthcareProvider/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
+aws s3 cp ./Scientist/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
+
+
