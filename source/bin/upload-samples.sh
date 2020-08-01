a24b7e223f0183d9081a0eea85dd9aae0c8aa3bd
Pierre <33883374+pierreaws@users.noreply.github.com>
2020-07-31 17:40:42 -0700

    Bulk covid (#81)
    * reorganized the kendra covid sample pdfs and json policies
    
    * reorganized the samples directories
    
    * covid bucket sync
    
    * reorganized the kendra covid sample pdfs and json policies
    
    * reorganized the samples directories
    
    * covid bucket sync
    
    Co-authored-by: Dumas <pierred@c4b301c672a3.ant.amazon.com>

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 source .env
-aws s3 cp ./samples/ s3://$SampleBucketName/public/samples/ --recursive
+aws s3 cp ./samples/ClassicMode/ s3://$SampleBucketName/public/samples/ --recursive
+aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+
+
