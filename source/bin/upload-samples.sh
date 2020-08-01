014df710f7de19846d57f50e2f5103c38e8d806b
Dumas <pierred@c4b301c672a3.ant.amazon.com>
2020-07-31 17:15:07 -0700

    covid bucket sync

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 source .env
-aws s3 cp ./samples/ s3://$SampleBucketName/public/samples/ --recursive
-
-aws s3 cp ./GeneralPublic/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
-aws s3 cp ./HealthcareProvider/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
-aws s3 cp ./Scientist/policy/ s3://$BulkProcessingBucketName/kendraPolicyDrop/ --recursive
-
-aws s3 cp ./GeneralPublic/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
-aws s3 cp ./HealthcareProvider/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
-aws s3 cp ./Scientist/pdf/ s3://$BulkProcessingBucketName/documentDrop/ --recursive
+aws s3 cp ./samples/ClassicMode/ s3://$SampleBucketName/public/samples/ --recursive
+aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+aws s3 sync s3://$CovidDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
 
 
