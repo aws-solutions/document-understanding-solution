0affedc834259b0732c00089fcf24b3f14e1c4fe
Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2020-10-05 10:53:08 -0700

    Additional refactoring

--- deployment/custom-deployment/bin/add_es_service_role_if_missing.sh
+++ deployment/custom-deployment/bin/add_es_service_role_if_missing.sh
@@ -2,5 +2,5 @@
 
 AWS_REGION=$(jq -r '.stack.region' ./package.json)
 
-echo "==Attempting to add service role to ElasticSearch in region $REGION=="
+echo "==Attempting to add service role to Amazon Elasticsearch in region $REGION=="
 $(aws iam create-service-linked-role --region $AWS_REGION --aws-service-name es.amazonaws.com && echo "Service role successfully added") || echo "Service role already exists"
