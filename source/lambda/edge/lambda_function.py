93e65841aeeddf82166bddce3d779603b7a1eb40
George Price <gwprice@amazon.com>
2020-10-15 16:45:06 -0400

    removed CSP header

--- source/lambda/edge/lambda_function.py
+++ source/lambda/edge/lambda_function.py
@@ -8,8 +8,8 @@ def lambda_handler(event, context):
     headers = response['headers']
     headers['strict-transport-security'] = [
         {'key': 'Strict-Transport-Security', 'value': 'max-age=63072000; includeSubdomains; preload'}]
-    headers['content-security-policy'] = [{'key': 'Content-Security-Policy',
-                                           'value': "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}]
+    # headers['content-security-policy'] = [{'key': 'Content-Security-Policy',
+    #                                        'value': "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}]
     headers['x-content-type-options'] = [
         {'key': 'X-Content-Type-Options', 'value': 'nosniff'}]
     headers['x-frame-options'] = [{'key': 'X-Frame-Options', 'value': 'DENY'}]
