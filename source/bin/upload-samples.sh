a9c01ea6cfb54228d344eaeb030cdaf8078bb75b
Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2020-10-22 15:59:43 -0700

    Revert "Revert medical pdfs (#132)"
    This reverts commit bea9e9083391fc50e8231b6b3919278db63c9fe1.

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -3,12 +3,12 @@ source .env
 aws s3 cp ./samples/ClassicMode/ s3://$SampleBucketName/public/samples/ --recursive
 if [ $ENABLE_KENDRA = "true" ]
 then
-    aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$CovidDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
 fi
 
 
