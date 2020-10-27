c2894cbb80f6aa635162b3eccc9d78e92e22a89c
Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2020-10-27 15:57:33 -0700

    Update upload-samples

--- source/bin/upload-samples.sh
+++ source/bin/upload-samples.sh
@@ -3,12 +3,13 @@ source .env
 aws s3 cp ./samples/ClassicMode/ s3://$SampleBucketName/public/samples/ --recursive
 if [ $ENABLE_KENDRA = "true" ]
 then
-    aws s3 sync s3://$MedicalDataBucketName/documents/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$MedicalDataBucketName/documents/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$MedicalDataBucketName/documents/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
-    aws s3 sync s3://$MedicalDataBucketName/documents/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$MedicalDataBucketName/documents/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
-    aws s3 sync s3://$MedicalDataBucketName/documents/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/GeneralPublic/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/HealthcareProvider/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/Scientist/policy/  s3://$BulkProcessingBucketName/kendraPolicyDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/GeneralPublic/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/HealthcareProvider/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/PersonaSpecific/Scientist/pdf/  s3://$BulkProcessingBucketName/documentDrop/
+    aws s3 sync s3://$MedicalDataBucketName/documents/GeneralDocuments/pdf/  s3://$BulkProcessingBucketName/documentDrop/
 fi
 
 
