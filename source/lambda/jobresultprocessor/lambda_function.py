6a2af84fa9d4f692a3fd180f9566765fe20ad20f
Alex Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2021-02-10 16:06:40 -0800

    Backend and infra changes

--- source/lambda/jobresultprocessor/lambda_function.py
+++ source/lambda/jobresultprocessor/lambda_function.py
@@ -149,12 +149,13 @@ def processRequest(request):
     print("Processed Comprehend data: {}".format(comprehendAndMedicalEntities))
 
     # index document once the comprehend entities and KVPairs have been extracted
-    for key, val in opg_output[KVPAIRS].items():
-        if key not in comprehendAndMedicalEntities:
-            comprehendAndMedicalEntities[key] = val
-        else:
-            comprehendAndMedicalEntities[key].add(val)
-    opg.indexDocument(opg_output[DOCTEXT], comprehendAndMedicalEntities)
+     if 'ES_DOMAIN' in os.environ :
+        for key, val in opg_output[KVPAIRS].items():
+            if key not in comprehendAndMedicalEntities:
+                comprehendAndMedicalEntities[key] = val
+            else:
+                comprehendAndMedicalEntities[key].add(val)
+        opg.indexDocument(opg_output[DOCTEXT], comprehendAndMedicalEntities)
 
     ds = datastore.DocumentStore(documentsTable, outputTable)
     ds.markDocumentComplete(documentId)
